a09540f136bcbaaa66fda46cefcfe856256be71d
Lucas Pereira da Silva <lucaszpsilva@gmail.com>
2024-01-29 20:30:37 -0300

    add hamburguer-menu and mediaquery
    Nessas atualizações eu deixei o site responsivo com mediaquery, designando um formato para cada tipo de media (desktop, tablet, smartphone) e adicionei o menu hamburguer para o modelo smartphone.

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ document.getElementById("radio1").checked = true;
 
 setInterval (function(){
     nextImage();
-}, 15000)
+}, 4000)
 
 function nextImage(){
     count++;
@@ -14,3 +14,11 @@ function nextImage(){
 
     document.getElementById("radio"+count).checked = true;
 }
+
+const menu_btn = document.querySelector('.hamburguer');
+const mobile_menu = document.querySelector('.mobile-nav')
+
+menu_btn.addEventListener('click', function(){
+    menu_btn.classList.toggle('is-active')
+    mobile_menu.classList.toggle('is-active')
+})
